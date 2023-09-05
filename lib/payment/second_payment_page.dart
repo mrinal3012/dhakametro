33d34902267e1341eefafa2aaf4f074116305799
mrinal3012 <mrinalpalit.cse@gmail.com>
2023-09-05 21:15:59 +0600

    add payment page

---
+++ lib/payment/second_payment_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SecondPaymentPage extends StatefulWidget {
+  const SecondPaymentPage({Key? key}) : super(key: key);
+
+  @override
+  State<SecondPaymentPage> createState() => _SecondPaymentPageState();
+}
+
+class _SecondPaymentPageState extends State<SecondPaymentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
