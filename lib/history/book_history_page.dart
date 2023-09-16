ec930a99649e24c99cb5c941f581cebe86684677
mrinal3012 <mrinalpalit.cse@gmail.com>
2023-09-16 23:01:36 +0600

    add history page

---
+++ lib/history/book_history_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BookHistoryPage extends StatefulWidget {
+  const BookHistoryPage({Key? key}) : super(key: key);
+
+  @override
+  State<BookHistoryPage> createState() => _BookHistoryPageState();
+}
+
+class _BookHistoryPageState extends State<BookHistoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
